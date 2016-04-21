790adb35b0e2229a44bfac2488e926b5f84aa702
Dave Bellagio <dbellagio@us.ibm.com>
2016-04-21 13:08:34 -0700

    Added Tests and Functions to implement FizzBuzz logic
    Added the tests and methods that will be needed to return the right data
    for FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,4 +7,19 @@ FizzBuzz.prototype.divisibleBy = function(number, divisor) {
 	return number % divisor === 0;
 };
 
+FizzBuzz.prototype.convertToFizzBuzz = function(number) {
+	// return "Buzz";
+	// return "Fizz";
+	if ( this.divisibleBy(number, 15) ) {
+		return "FizzBuzz";
+	}
+	if ( this.divisibleBy(number, 5) ) {
+		return "Buzz";
+	}
+	if ( this.divisibleBy(number, 3) ) {
+		return "Fizz";
+	}
+	return number.toString();
+};
+
 module.exports = FizzBuzz;
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -9,6 +9,27 @@ describe("Fizzbuzz", function () {
 		});
 		it("when not divisible", function() {
 			expect(f.divisibleBy(3, 2)).to.be.eql(false);
-		})
+		});
 	});
+	
+	describe("convertToFizzBuzz()", function() {
+		it("when divisible by 3", function() {
+			expect(f.convertToFizzBuzz(3)).to.be.eql("Fizz");
+			expect(f.convertToFizzBuzz(6)).to.be.eql("Fizz");
+		});		
+		it("when divisible by 5", function() {
+			expect(f.convertToFizzBuzz(5)).to.be.eql("Buzz");
+			expect(f.convertToFizzBuzz(10)).to.be.eql("Buzz");
+		});	
+		it("when divisible by 15", function() {
+			expect(f.convertToFizzBuzz(15)).to.be.eql("FizzBuzz");
+			expect(f.convertToFizzBuzz(30)).to.be.eql("FizzBuzz");
+		});	
+		
+		it("when not divisible by 3, 5 or 15", function () {
+			expect(f.convertToFizzBuzz(4)).to.be.eql("4");
+			expect(f.convertToFizzBuzz(7)).to.be.eql("7");
+		});
+	});
+	
 })
